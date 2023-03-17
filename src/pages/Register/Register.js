639ad355aa8d85dd5f2b221b26b74c0bb2fd6aee
joao vitor <vl351962@gmail.com>
2023-03-17 16:51:47 -0400

    alteraçao feita

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -9,7 +9,7 @@ const Register = () => {
   const [displayname, setDisplayname] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState(""); 
   const [error, setError] = useState("");
   
   const handleSubmit = (event) => {
