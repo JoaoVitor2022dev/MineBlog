953e023986ad84a5ac287225201e8e233dbbd9fd
joao vitor <githubconta2021@gmail.com>
2023-04-06 16:48:20 -0400

    finalizando o code style da pagina home

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -4,18 +4,16 @@ import { Link } from "react-router-dom"
 
 const PostDetail = ( { post } ) => {
   return (
-    <div>
+    <div className={styles.post_detail}>
          <img src={post.image} alt={post.title}/>
          <h2>{post.title}</h2>
-         <p>{post.createdBy}</p>
-         <p>{post.body}</p>
-         <div>
+         <p className={styles.createdby}>{post.createdBy}</p>
+         <div className={styles.tags}>
              {post.tagsArray.map((tag) => (
                 <p key={tag}><span>#</span>{tag}</p>
              ))}
          </div>
-         <Link to={`/posts/${post.id}`} className="btn btn-outline">Ler</Link>
-         
+         <Link to={`/posts/${post.id}`} className="btn btn-outline">Ler</Link>     
     </div>
   )
 }
