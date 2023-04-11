27f9d8721aed97fa8a4fde82b915e69f1b5f4735
joao vitor <githubconta2021@gmail.com>
2023-04-11 17:47:29 -0400

    conecntando com o back and a pate de um só post...

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -3,13 +3,23 @@ import styles from "./Post.module.css"
 // hooks 
 import { useParams } from "react-router-dom";
 
+// hook de fetch 
+import { useFetchDocument } from "../../hook/useFetchDocument";
+
 
 const Post = () => {
   const { id } = useParams(); 
+
+  const { document: post, loading } = useFetchDocument("posts", id)
   
   return (
     <div>
-        <h1>Posts - {id}</h1>
+        {post && (
+          <>
+          {loading && <p>Carregamento post... </p>}
+           <h1>{post.title}</h1>
+          </>
+        )}
     </div>
   )
 }
