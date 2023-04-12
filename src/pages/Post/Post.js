43fec53fbe9e9378a913f2b1d7334749feafa536
joao vitor <githubconta2021@gmail.com>
2023-04-12 18:15:42 -0400

    styles da page do post

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -10,14 +10,22 @@ import { useFetchDocument } from "../../hook/useFetchDocument";
 const Post = () => {
   const { id } = useParams(); 
 
-  const { document: post, loading } = useFetchDocument("posts", id)
+  const { document: post, loading } = useFetchDocument("posts", id); 
   
   return (
-    <div>
+    <div className={styles.post_container}>
+      {loading && <p>Carregamento post... </p>}
         {post && (
           <>
-          {loading && <p>Carregamento post... </p>}
            <h1>{post.title}</h1>
+           <img src={post.image} alt={post.title}/>
+           <p>{post.body}</p>
+           <h3>Este post trata sobre:</h3>
+           <div className={styles.tags}>
+           {post.tagsArray.map((tag) => (
+                <p key={tag}><span>#</span>{tag}</p>
+             ))}
+           </div>
           </>
         )}
     </div>
