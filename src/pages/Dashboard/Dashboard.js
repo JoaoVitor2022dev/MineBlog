b835553abf5d0abef3b4b5ea10fac82654a80806
joao vitor <githubconta2021@gmail.com>
2023-04-14 18:49:07 -0400

    desenvolvendo a pagina do dashboar

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,12 +1,69 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+import { useAuthValue } from "../../Context/AuthContext";
+import { useFetchDocuments } from "../../hook/useFetchDocuments";
+
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const deleteDocument = (id) => {
+
+  }
+  
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
+  console.log(uid);
+  console.log(posts);
+
   return (
-    <div>
-        <h1>Dashbord</h1>
+    <div className={styles.dashboard}>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <>
+         <div>
+            <span>Titulos</span>
+            <span>Acoes</span>
+         </div>
+         {posts &&
+        posts.map((post) => (
+          <div className={styles.post_row} key={post.id}>
+            <p>{post.title}</p>
+            <div className={styles.actions}>
+              <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                Ver
+              </Link>
+              <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">
+                Editar
+              </Link>
+              <button
+                onClick={() => deleteDocument(post.id)}
+                className="btn btn-outline btn-danger"
+              >
+                Excluir
+              </button>
+            </div>
+          </div>
+        ))}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Dashboard;
-
